16025c50dc878f57077a1cd9e5ec1cd266ab4489
WojtekBaranowski001 <wojtekbaranowski9@gmail.com>
2022-03-22 14:33:13 +0000

    Update 2 - phone n dat

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,24 @@
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
+// nav burger selection 
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
 
 scrollUp.addEventListener("click", () => {
   window.scrollTo({
